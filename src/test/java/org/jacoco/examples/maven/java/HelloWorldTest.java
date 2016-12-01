aec00a605d56fce8b1450f0276569e79f6b38dfc
t616178 <t616178@users.noreply.github.com>
2016-12-01 18:44:01 +0000

    Add other test case

--- src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
+++ src/test/java/org/jacoco/examples/maven/java/HelloWorldTest.java
@@ -19,4 +19,8 @@ public class HelloWorldTest {
 		assertEquals("Hello World!", subject.getMessage(false));
 	}
 	
+	@Test
+	public void testGetMessage() {
+		assertEquals("Hello World!", subject.getMessage(true));
+	}
 }
